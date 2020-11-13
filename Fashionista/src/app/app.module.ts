969ed59c699f72b222a93559df65fe642d53f01a
Jeremy Suero <44537895+iamyobany@users.noreply.github.com>
2020-11-13 15:45:17 -0500

    added new components and services

--- Fashionista/src/app/app.module.ts
+++ Fashionista/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/home/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
--- Fashionista/src/app/components/login/login.component.ts
+++ Fashionista/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
+import { UserService } from './../../services/user/user.service';
+import { AuthService } from './../../services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  failedLogin;
+
+  constructor(private readonly fb: FormBuilder, private auth: AuthService, private router: Router, private us: UserService) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
+    });
+    this.failedLogin = false;
+  }
 
   ngOnInit(): void {
+  }
+
+  submitLoginForm(): void {
+    if (this.form.valid) {
+      // console.log(this.form.getRawValue());
+      this.auth.login(this.form.getRawValue()).then(status => {
+        if (!status) {
+          console.log(status);
+          // console.log('Failed login.');
+          this.failedLogin = true;
+        } else {
+          // console.log('Successful login.');
+          this.us.getUserById(this.auth.user._id).then(() => this.router.navigate([`/${this.auth.user.type}`, {trigger: 'SIGN_IN'}]));
+
+        }
+      });
+    }
+
   }
 
 }

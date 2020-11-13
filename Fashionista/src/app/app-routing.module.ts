4f3108a565eacb75d1ae5dc7d72b0dfed95ad877
Jeremy Suero <44537895+iamyobany@users.noreply.github.com>
2020-11-13 15:35:04 -0500

    added login and register components

--- Fashionista/src/app/app-routing.module.ts
+++ Fashionista/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

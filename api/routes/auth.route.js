2d283d5840a2feffe504ae38120b7f73e3444126
Jeremy Suero <44537895+iamyobany@users.noreply.github.com>
2020-11-13 15:19:25 -0500

    added auth route

---
+++ api/routes/auth.route.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const User = require('../models/user.schema')
+
+router.post('/login', async (req, res) => {
+    const data = req.body
+    const user = await User.findOne({email: data.email, password:data.password})
+    if(!user){
+        res.status(401).json({error: 'Database Error: No User Found'})
+    }
+
+    res.status(200).json(user)
+})
+
+module.exports = router;
